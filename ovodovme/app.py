89a96cd23506de7625f3ebe3c57baf3f0e34a1f9
Boris Ovodov <boris@ovodov.me>
2018-05-31 17:31:47 +0500

    Переезд на Flask

--- ovodovme/app.py
+++ ovodovme/app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "Hello World!"
+	return render_template('index.html')
+
+@app.route('/trud')
+def trud():
+	return render_template('trud.html')
+
+@app.route('/trud.ics')
+def trudics():
+	return 'trud.ics'
+
+@app.route('/id_rsa.pub')
+def idrsa():
+	return 'id_rsa.pub'
+
