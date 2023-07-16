9a809e358d5dcfadebcef7e7dee14ac9ffb5ef64
borisovodov <boris@ovodov.me>
2023-07-16 22:09:10 +0500

    ...

--- static/clipboard.js
+++ static/clipboard.js
@@ -2,12 +2,27 @@ function addToClipboard() {
   navigator.clipboard.writeText("https://ovodov.me/trud.ics");
 }
 
-function myFunction() {
-  var checkBox = document.getElementById("flexSwitchCheckDefault");
-  var text = document.getElementById("authText");
-  if (checkBox.checked == true){
-    text.style.display = "block";
-  } else {
-     text.style.display = "none";
-  }
+var checkbox = document.getElementById('authSwitch')
+
+var authOff = document.getElementsByClassName("authOff")
+var authOn = document.getElementsByClassName("authOn")
+
+function checker(checked) {
+  checked ? showAuth() : hideAuth()
 }
+
+function showAuth() {
+  authOff[0].style.display = "none"
+  authOn[0].style.display = "inline-block"
+}
+
+function hideAuth() {
+  authOff[0].style.display = "inline-block"
+  authOn[0].style.display = "none"
+}
+
+checker(this.checked)
+
+checkbox.addEventListener('change', function () {
+  checker(this.checked)
+})
