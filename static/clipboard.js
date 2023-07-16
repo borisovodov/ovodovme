46c2ece35c29d1743d99a239da3a89886e7498f5
borisovodov <boris@ovodov.me>
2023-07-16 21:54:51 +0500

    ...

--- static/clipboard.js
+++ static/clipboard.js
@@ -1,3 +1,13 @@
 function addToClipboard() {
   navigator.clipboard.writeText("https://ovodov.me/trud.ics");
 }
+
+function myFunction() {
+  var checkBox = document.getElementById("authSwitch");
+  var text = document.getElementById("authText");
+  if (checkBox.checked == true){
+    text.style.display = "block";
+  } else {
+     text.style.display = "none";
+  }
+}
