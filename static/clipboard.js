151ba35024424a5fd313a332a74e9ec15b04d7be
borisovodov <boris@ovodov.me>
2023-07-16 22:32:22 +0500

    ...

--- static/clipboard.js
+++ static/clipboard.js
@@ -4,31 +4,28 @@ function addToClipboard() {
 
 var checkbox = document.getElementById('authSwitch')
 
-var authOff = document.getElementsByClassName("authOff")
-var authOn = document.getElementsByClassName("authOn")
+//var authOff = document.getElementsByClassName("authOff")
+//var authOn = document.getElementsByClassName("authOn")
 
-function checker() {
-  if ($("#authSwitch").is(":checked")) {
-    authOff[0].style.display = "none"
-    authOn[0].style.display = "inline-block"
-  } else {
-    authOff[0].style.display = "inline-block"
-    authOn[0].style.display = "none"
-  }
+var authOff = document.getElementById("authOff")
+var authOn = document.getElementById("authOn")
+
+function checker(checked) {
+  checked ? showAuth() : hideAuth()
 }
 
 function showAuth() {
-  authOff[0].style.display = "none"
-  authOn[0].style.display = "inline-block"
+  authOff.style.display = "none"
+  authOn.style.display = "inline-block"
 }
 
 function hideAuth() {
-  authOff[0].style.display = "inline-block"
-  authOn[0].style.display = "none"
+  authOff.style.display = "inline-block"
+  authOn.style.display = "none"
 }
 
-checker()
+checker(this.checked)
 
 checkbox.addEventListener('change', function () {
-  checker()
+  checker(this.checked)
 })
