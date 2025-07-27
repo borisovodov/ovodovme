bdc3b127965486934b111d192eb2eafdce36ce76
Boris Ovodov <boris@ovodov.me>
2025-07-27 12:23:19 +0500

    Добавил страницу без адреса

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+import "@/app/globals.css";
+
+import Link from "next/link";
+
+export default function NotFound() {
+    return (
+        <div className="flex items-center justify-center min-h-screen">
+            <div className="text-5xl">
+            <h2>Ну ёб твою мать, ну что это такое...</h2>
+            <Link className="text-blue-600 hover:text-blue-700 underline" href="/">Вернуться на главную</Link>
+            </div>
+        </div>
+    )
+}
