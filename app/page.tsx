0633423cd440c104e93290b7f96b2e50d92d8980
Boris Ovodov <boris@ovodov.me>
2025-10-09 12:21:21 +0500

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -6,34 +6,28 @@ import {
 	Tab,
 	Tabs
 } from "@heroui/react"
+// import { fetchWeatherApi } from "openmeteo";
+// import { useState, useEffect } from "react"
 
+// import { data } from "@/data"
 import { Link, PageHeader, Signature } from "@/components"
 
 export default function HomePage() {
 	return (
 		<div className="max-w-2xl">
 			<PageHeader title="Всем привет от Бориса Оводова 👋🏻" />
-			{/* <Card shadow="sm">
-				<CardBody className="text-gray-400">
-					Екатеринбург, 17:41, ⛅️
-					<br />
-					Группа крови? • R1a-M417 (R1a1a1) • Z1a1a
-					<br />
-					D80B 2855 595A 0BAF FE09  0DC1 03D4 F66F D856 A100
-					FQKf8ftw4JTgHeyhzFyL8yj5F64Z4bC7+DJFWuzUn4s
-					<br />
-					Добиться группу крови, текущий город, погоду в текущем городе, время локальное в текущем городе, Y и mt гаплогруппы, сколько жить осталось, PGP ключ
-				</CardBody>
-			</Card> */}
-			<Tabs fullWidth aria-label="Сферы" size="lg" className="mt-4">
+			{/* <PersonalInfo /> */}
+			<Tabs fullWidth aria-label="Сферы" size="lg" className="mt-8">
 				<Tab key="projects" title="Проекты">
 					<p className="mt-4">Разрабатываю «<Link href="https://hermann.ovodov.me/">Германн</Link>» — тайм-трекер без трения.</p>
 					<p className="mt-4">Собираю газеты со всего света и веду об этом англоязычный <Link href="https://papersaround.com/">вебсайт</Link>.</p>
 					<p className="mt-4">Поддерживаю <i>нормальный</i> <Link href="/trud">производственный календарь</Link> в формате айкол.</p>
 					<p className="mt-4">Пишу субъективный <Link href="/ye">путеводитель</Link> по культовым местам Екатеринбурга.</p>
 					<p className="mt-4">Сделал <Link href="/event">генератор</Link> iCal-файлов с календарными событиями из URL.</p>
+				</Tab>
+				<Tab key="media" title="Медийка">
+					<p className="mt-4">Рассказываю истории про науку, общество и прочие щщи в подкастах студии «<Link href="https://podcasts.apple.com/ru/channel/%D0%B1%D0%BE%D1%80%D0%B8%D1%8F-%D0%B8-%D1%80%D0%B5%D0%B1%D1%8F%D1%82%D0%B0/id6445257341?l=en">Боря и ребята</Link>».</p>
 					<p className="mt-4">Опубликовал <Link href="/diary">письмо</Link> двоюродного деда.</p>
-					<p className="mt-4">Рассказываю истории про науку, общество и прочие щщи в подкастах студии «<Link href="https://podcasts.apple.com/ru/channel/%D0%B1%D0%BE%D1%80%D1%8F-%D0%B8-%D1%80%D0%B5%D0%B1%D1%8F%D1%82%D0%B0/id6445257341?l=en">Боря и ребята</Link>».</p>
 					<p className="mt-8">Веду несколько уютных Телеграм-каналов:</p>
 					<ul className="list-disc list-inside">
 						<li className="mt-2"><Link href="https://t.me/safariBookmarks">Борины закладки в Сафари</Link> — перфомансы и анонсы проектов.</li>
@@ -43,7 +37,7 @@ export default function HomePage() {
 						<li className="mt-2"><Link href="https://t.me/hermannapp">Hermann</Link> — новости лучшего тайм-трекера на этой планете.</li>
 					</ul>
 				</Tab>
-				<Tab key="talks" title="Выступления">
+				<Tab key="talks" title="Паблик токи">
 					<p className="mt-4"><Link href="https://www.youtube.com/watch?v=A6u0JiTCYpc">Интервью</Link> для IT-подкаста Саши Торбека, 2025.</p>
 					<p className="mt-4">Кейс-клубы с экспертами на Контуровской конференции аналитиков в <Link href="https://www.youtube.com/live/vJi1IsFgQqU?si=Jg2TI6bCQ0PqM98Z&t=7450">2023-м</Link> и <Link href="https://youtu.be/6ex2IZJ6CCw?t=5669">2024-м</Link> годах.</p>
 					<p className="mt-4">Доклад «<Link href="https://www.youtube.com/watch?v=YJRmndQbBG8">Почему аналитикам стоит делать пет-проекты</Link>» для ютуб-канала Наумена, 2023. Его же <Link href="https://youtube.com/watch?v=zeOJ6WIpv78">рассказывал</Link> на екатеринбургском ДАМПе в том же году.</p>
@@ -64,17 +58,14 @@ export default function HomePage() {
 					<p className="mt-4"><Link href="https://www.youtube.com/@borisovodovlive">Ютуб</Link></p>
 				</Tab>
 				<Tab key="past" title="Позади">
-					<p className="mt-4">Снимал фильмы на <Link href="/28studio.png">28-й студии</Link>.</p>
-					<p className="mt-4">Сочинял стихи юности на «<Link href="http://pisanina.su">Писанине</Link>».</p>
-					<p className="mt-4">Играл в <Link href="https://cybico.bandcamp.com/music">КYБ и Ко</Link>.</p>
-					<p className="mt-4">Разработал <Link href="https://www.dropbox.com/s/owx6ddrix3gepcj/%D0%9C%D0%B5%D1%82%D0%BE%D0%B4%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B5%20%D0%BF%D0%BE%D1%81%D0%BE%D0%B1%D0%B8%D0%B5%20Quantum%20GIS.zip?dl=0">методичку</Link> для обучения студентов основам работы в ГИС.</p>
-					<p className="mt-4">Писал статьи в <Link href="https://vk.com/revmus">Ревмузе</Link>.</p>
 					<p className="mt-4">Преподавал астрономию в екатеринбургской <Link href="https://xn--35-6kclvec3aj7p.xn--80acgfbsl1azdqr.xn--p1ai/">35-й гимназии</Link>. В это время подготовил <Link href="/astroege.pdf">методичку по астрономии</Link> для успешной сдачи ЕГЭ по физике.</p>
+					<p className="mt-4">Писал статьи в <Link href="https://vk.com/revmus">Ревмузе</Link>.</p>
+					<p className="mt-4">Разработал <Link href="https://www.dropbox.com/s/owx6ddrix3gepcj/%D0%9C%D0%B5%D1%82%D0%BE%D0%B4%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B5%20%D0%BF%D0%BE%D1%81%D0%BE%D0%B1%D0%B8%D0%B5%20Quantum%20GIS.zip?dl=0">методичку</Link> для обучения студентов основам работы в ГИС.</p>
+					<p className="mt-4">Играл в <Link href="https://cybico.bandcamp.com/music">КYБ и Ко</Link>.</p>
+					<p className="mt-4">Сочинял стихи юности на «<Link href="http://pisanina.su">Писанине</Link>».</p>
+					<p className="mt-4">Снимал фильмы на <Link href="/28studio.png">28-й студии</Link>.</p>
 				</Tab>
 				<Tab key="future" title="Впереди">
-					<p className="mt-4">Проведу перфоманс с важными извинениями.</p>
-					<p className="mt-4">Сниму документальный фильм про то, как трое ребят ищут свои корни.</p>
-					<p className="mt-4">Напишу книгу про Онисима Клера и его семью.</p>
 					<p className="mt-4">Проживу жизнь, наполненную до краёв.</p>
 				</Tab>
 			</Tabs>
@@ -82,3 +73,55 @@ export default function HomePage() {
 		</div>
 	);
 }
+
+// export function PersonalInfo() {
+// 	const { city, timeZone } = data
+// 	const [time, setTime] = useState(new Date())
+
+// 	useEffect(() => {
+// 		const interval = setInterval(() => setTime(new Date()), 1000)
+// 		return () => clearInterval(interval)
+// 	}, [])
+
+// 	const timeString = time.toLocaleTimeString("ru-RU", {
+// 		hour12: false,
+// 		timeZone,
+// 	})
+
+// 	return (
+// 		<Card shadow="sm">
+// 			<CardBody className="text-gray-400 text-sm">
+// 				{city}, {timeString}, ⛅️
+// 				<br />
+// 				R1a-M417 (R1a1a1) • Z1a1a
+// 				<br />
+// 				D80B 2855 595A 0BAF FE09  0DC1 03D4 F66F D856 A100
+// 				<br />
+// 				FQKf8ftw4JTgHeyhzFyL8yj5F64Z4bC7+DJFWuzUn4s
+// 				<br />
+// 				Осталось ~ 60,2 лет
+// 			</CardBody>
+// 		</Card>
+// 	);
+// }
+
+// export async function Weather() {
+// 	const weatherParams = {
+// 		latitude: data.latitude,
+// 		longitude: data.longitude,
+// 		current: ["weather_code", "temperature_2m"],
+// 	};
+// 	const url = "https://api.open-meteo.com/v1/forecast";
+// 	const responses = await fetchWeatherApi(url, weatherParams);
+// 	const current = responses[0].current();
+
+// 	if (current) {
+// 		const weatherData = {
+// 			current: {
+// 				time: new Date((Number(current.time()) + utcOffsetSeconds) * 1000),
+// 				weather_code: current.variables(0)!.value(),
+// 				temperature_2m: current.variables(1)!.value(),
+// 			},
+// 		};
+// 	}
+// }
---
+++ data.ts
@@ -0,0 +1,6 @@
+export const data = {
+    city: "Екатеринбург",
+    latitude: 52.52,
+    longitude: 13.41,
+    timeZone: "Asia/Yekaterinburg",
+}
