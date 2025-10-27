fb9fa6867e840e81df8961f50597e5e88de47b9b
Boris Ovodov <boris@ovodov.me>
2025-10-27 09:33:08 +0500

    Update data.ts

--- data.ts
+++ data.ts
@@ -1,6 +1,13 @@
 export const data = {
-    city: "Большеустьикинское",
-    latitude: 55.949622,
-    longitude: 58.267263,
+    city: "Екатеринбург",
+    latitude: 56.8519,
+    longitude: 60.6122,
     timeZone: "Asia/Yekaterinburg",
 }
+
+//export const data = {
+//    city: "Большеустьикинское",
+//    latitude: 55.949622,
+//    longitude: 58.267263,
+//    timeZone: "Asia/Yekaterinburg",
+//}
